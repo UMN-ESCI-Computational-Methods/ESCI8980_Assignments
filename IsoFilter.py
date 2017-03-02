101feb8e66747321e3ca7dfd05872ba368ea4cb2
junissen <nisse053@umn.edu>
2017-03-01 23:26:02 -0600

    Add files via upload
    Added working files for IsoFilter and calling function

---
+++ IsoFilter.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Class IsoFilter created containing functions to calculate mean, 
+standard deviation, total counts, and filter data outside of specified 
+range.
+
+Created by Nick Wang and Julia Nissen, 2017.
+
+"""
+
+import openpyxl
+import numpy as np
+
+class IsoFilter(object):
+    def __init__(self, filename, columnletter): # input filename and columnletter as strings
+        self.column = str(columnletter)+'{}:'+str(columnletter)+'{}'
+        self.filename = str(filename)+'.xlsm'
+        self.workbook = openpyxl.load_workbook(self.filename)
+        self.ws = self.workbook.active
+        self.totalCounts = 0
+        self.mean = 0 
+        self.filteredMean = 0
+        self.err = 0
+        self.criteria = 0
+        
+ 
+    def getMean(self):
+        outlist = []
+        for row in self.ws.iter_rows(self.column.format(2, self.ws.max_row - 8)):
+            for cell in row:
+                if cell.value:
+                    outlist.append(cell.value)
+                else:
+                    outlist.append(np.nan)
+        outarray = np.array(outlist, dtype = np.float) 
+        self.mean = np.nanmean(a = outarray)
+
+
+    def getStanddev(self):
+        outlist = []
+        for row in self.ws.iter_rows(self.column.format(2, self.ws.max_row - 8)):
+            for cell in row:
+                value = cell.value
+                if value:
+                    outlist.append(value)
+                else:
+                    outlist.append(np.nan)
+        outarray = np.array(outlist, dtype = np.float) 
+        self.standdev = np.nanstd(a = outarray)
+        return self.standdev
+    
+    def getCounts(self):
+        total_counts = 0
+        for row in self.ws.iter_rows(self.column.format(2, self.ws.max_row -8)):
+            for cell in row:
+                value = cell.value
+                if value:
+                    total_counts += 1
+
+        self.totalCounts = total_counts
+        return self.totalCounts
+        
+    def theFilter(self):
+        self.criteria = 44 * (self.standdev / self.totalCounts ** 0.5)
+        outlist = []
+        outcounts = 0
+        for row in self.ws.iter_rows(self.column.format(2, self.ws.max_row - 8)):
+            for cell in row:
+                value = cell.value
+                if value:
+                    if abs(value - self.mean) > self.criteria:
+                        outlist.append(np.nan)
+                    else:
+                        outlist.append(value)
+                        outcounts += 1
+                else:
+                    outlist.append(np.nan)
+        outarray = np.array(outlist, dtype = np.float)
+        self.filteredMean = np.nanmean(a = outarray)
+        outstanddev = np.nanstd(a = outarray)
+        self.err = 2 * (outstanddev / (outcounts ** 0.5))
+        print 'Filtering done! '
+        
+        
+    def __repr__(self):
+        
+        return 'Filtered mean: '+str(self.filteredMean)+'\n' + \
+        'Filtered 2s error: '+ str(self.err)
+        
+        
+     
+    
+        
---
+++ Iso_main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Function main which gives specified outputs of the IsoFilter class. 
+
+Created by Nick Wang and Julia Nissen, 2017
+
+"""
+
+import IsoFilter
+
+def main():
+    print("Standard U_filter")
+    filename = raw_input("Enter the source file name: ")
+    columnletter = raw_input("What column are we looking at? ")
+    
+    i = 1
+    validEntry = False
+    while i<3:
+        while not validEntry:
+            try:
+                wb = IsoFilter.IsoFilter(str(filename),str(columnletter))
+                validEntry = True     
+            except IOError:
+                filename = raw_input("No such file exist. Enter the correct source file name: ")
+        i += 1
+    wb.getMean()
+    wb.getStanddev()
+    wb.getCounts()
+    wb.theFilter()
+    print(wb)
+    
+    
+            
+
+       
